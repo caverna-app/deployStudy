8103a38284a238d083229ca40f6154670ee3c1ee
Rodrigo Cezar Leão <rodrigo.leao@radixeng.com.br>
2023-03-14 11:31:48 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: C1Component},
+  {path: "cailou", component: C2Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
